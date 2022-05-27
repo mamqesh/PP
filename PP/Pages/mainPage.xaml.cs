818a9ab524d0840a6fbce86cc0019d573196a7bb
mamqesh <miklushova4556@mail.ru>
2022-05-27 11:18:23 +0300

    upd 27.05

--- PP/Pages/mainPage.xaml.cs
+++ PP/Pages/mainPage.xaml.cs
@@ -23,10 +23,50 @@ namespace PP.Pages
     public partial class mainPage : Page
     {
         danil3Entities connection = new danil3Entities();
+        public List<Database.Unit> units { get; set; }
+        public Database.Product product { get; set; }
 
         public mainPage()
         {
             InitializeComponent();
+            LoadProductID();
+            units = connection.Unit.ToList();
+            DataContext = this;
+            
+        }
+
+        void LoadProductID()
+        {
+            int productID = connection.Product.ToList().Count() + 1;
+            textBoxIDProduct.Text = productID.ToString();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)//ДОБАВИТЬ ПРОДУКЦИЮ
+        {
+            string numberProduct = textBoxNumberProduct.Text.Trim();
+            string nameProduct = textBoxNameProduct.Text.Trim();
+            string countProduct = textBoxCountProduct.Text.Trim();
+            string noteProduct = textBoxNoteProduct.Text.Trim();
+            void ClearText()
+            {
+                numberProduct = "";
+                nameProduct = "";
+                countProduct = "";
+                noteProduct = "";
+                textBoxNumberProduct.Clear();
+                textBoxNameProduct.Clear();
+                textBoxCountProduct.Clear();
+                textBoxNoteProduct.Clear();
+            }
+            if (numberProduct.Length == 0 || nameProduct.Length == 0||countProduct.Length==0||noteProduct.Length==0)
+            {
+                MessageBox.Show("Не все данные введенны");
+                return;
+            }
+            else
+            {
+                Database.Product product = new Database.Product();
+                
+            }
         }
     }
 }
