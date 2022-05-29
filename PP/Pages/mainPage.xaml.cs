fee93a353610f64b75557fa2cb8bcf82c7959942
mamqesh <miklushova4556@mail.ru>
2022-05-29 19:42:14 +0300

    upd 29.05

--- PP/Pages/mainPage.xaml.cs
+++ PP/Pages/mainPage.xaml.cs
@@ -85,5 +85,10 @@ namespace PP.Pages
                 }
             }
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Open
+        }
     }
 }
